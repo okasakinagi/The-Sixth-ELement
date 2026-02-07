39c95e77ba81706a42fa4e95228c541f75ead54b
岡崎凪 <141289020+okasakinagi@users.noreply.github.com>
2026-02-07 15:26:46 +0800

    部署更新
    已更新requirement.txt

--- module/survey_app/urls.py
+++ module/survey_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
+from django.http import HttpResponse
 from core import views
+
+
+def healthz(request):
+    # 简单的健康检查端点，返回 HTTP 200 表示服务存活
+    return HttpResponse("ok")
+
 
 urlpatterns = [
     path("", views.index),
+    path("healthz", healthz),
     path("api/v1/", include("core.urls")),
     path("api/v1/", include("personal_homepage.urls")),
     path("api/v1/", include("survey_management.urls")),
